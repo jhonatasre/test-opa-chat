d6fa5a6f9ad6e4e6c609e10e5e35677c99b5e23c
Jhonatas Reis Matos <jhonatasre@gmail.com>
2024-10-18 17:29:29 -0300

    scroll

--- app/app/chat/page.js
+++ app/app/chat/page.js
@@ -2,9 +2,9 @@
 
 import md5 from 'md5';
 import moment from 'moment';
-import { useEffect, useState } from 'react';
 import { useAuth } from '../context/AuthContext';
 import { useToast } from '../context/ToastContext';
+import { useEffect, useState, useRef } from 'react';
 import PrivateRoute from '../components/PrivateRoute';
 
 import * as Icon from 'react-bootstrap-icons';
@@ -18,6 +18,7 @@ export default function Chat() {
     const [userActive, setUserActive] = useState({ id: '', name: '', username: '' });
 
     const { showToast } = useToast();
+    const messagesEndRef = useRef(null);
 
     const fetchUsers = async () => {
         try {
@@ -35,13 +36,10 @@ export default function Chat() {
             setUsers(usersData);
         } catch (err) {
             return showToast('Erro', `Erro: ${err?.message}`, 'warning', 10000);
-        } finally {
-            // setLoading(false);
         }
     };
 
     const handleSelectUser = async (user) => {
-
         try {
             const res = await fetch(`http://localhost:3001/chat/${user.id}`, {
                 headers: {
@@ -81,6 +79,11 @@ export default function Chat() {
                 return showToast('Erro', 'Erro ao enviar mesnagem', 'warning', 10000);
             }
 
+            setListMessages(listMessages => [...listMessages, {
+                sender: user.id,
+                content: message,
+                timestamp: moment().format()
+            }]);
             setMessage('');
         } catch (err) {
             return showToast('Erro', `Erro: ${err?.message}`, 'warning', 10000);
@@ -91,118 +94,126 @@ export default function Chat() {
         fetchUsers();
     }, []);
 
+    useEffect(() => {
+        if (messagesEndRef.current) {
+            messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
+        }
+    }, [listMessages]);
+
     return (
         <PrivateRoute>
-            <div style={{ backgroundImage: "url('/img/background.jpg')" }} className="min-vh-100">
-                <Container className="d-flex w-100 flex-column justify-content-center min-vh-100">
-                    <Card style={{ borderRadius: 35 }}>
-                        <Card.Header>
-                            <Row>
-                                <Col xs={10}>
+            {
+                !user ? <></> :
+                    <div style={{ backgroundImage: "url('/img/background.jpg')" }} className="min-vh-100">
+                        <Container className="d-flex w-100 flex-column justify-content-center min-vh-100">
+                            <Card style={{ borderRadius: 35 }}>
+                                <Card.Header>
                                     <Row>
-                                        <Col xs={2} lg={1}>
-                                            {user ? (
-                                                <Image src={`https://www.gravatar.com/avatar/${md5(user.username)}?d=identicon`} roundedCircle fluid />
-                                            ) : (
-                                                <Image src="/img/avatar.png" roundedCircle fluid />
-                                            )}
+                                        <Col xs={10}>
+                                            <Row>
+                                                <Col xs={2} lg={1}>
+                                                    <Image src={`https://www.gravatar.com/avatar/${md5(user.username)}?d=identicon`} roundedCircle fluid />
+                                                </Col>
+                                                <Col xs={10} lg={11} className="d-flex flex-column justify-content-center">
+                                                    <span><Icon.CircleFill className="text-success" /> {user.name}</span><br />
+                                                    <small>@{user.username}</small>
+                                                </Col>
+                                            </Row>
                                         </Col>
-                                        <Col xs={10} lg={11} className="d-flex flex-column justify-content-center">
-                                            <span><Icon.CircleFill className="text-success" /> {user ? user.name : ''}</span><br />
-                                            <small>@{user ? user.username : ''}</small>
+                                        <Col xs={2} className="text-end align-items-center">
+                                            <Button onClick={() => logout()} variant="link" size="lg" className="text-danger" title="Sair">
+                                                <Icon.Power className="font-weight-bolder fs-3" />
+                                            </Button>
                                         </Col>
                                     </Row>
-                                </Col>
-                                <Col xs={2} className="text-end align-items-center">
-                                    <Button onClick={() => logout()} variant="link" size="lg" className="text-danger" title="Sair">
-                                        <Icon.Power className="font-weight-bolder fs-3" />
-                                    </Button>
-                                </Col>
-                            </Row>
-                        </Card.Header>
-                        <Card.Body className="m-0">
-                            <Row className="h-vh-75">
-                                <Col md={4}>
-                                    <ListGroup>
-                                        {users.map((user) => (
-                                            <ListGroup.Item action onClick={() => handleSelectUser(user)}>
-                                                <Row>
-                                                    <Col xs={4} lg={2} className="d-flex flex-column justify-content-center">
-                                                        <Image src={`https://www.gravatar.com/avatar/${md5(user.username)}?d=identicon`} roundedCircle fluid />
-                                                    </Col>
-                                                    <Col xs={8} lg={10} className="d-flex flex-column justify-content-center">
-                                                        <span><Icon.CircleFill /> {user.name}</span><br />
-                                                        <small>@{user.username}</small>
-                                                    </Col>
-                                                </Row>
-                                            </ListGroup.Item>
-                                        ))}
-                                    </ListGroup>
-                                </Col>
-                                <Col md={8} className="d-flex flex-column justify-content-between">
-                                    <div className="flex-grow-1">
-                                        <Row className="mb-2">
-                                            <Col xs={1} className="d-flex flex-column justify-content-center">
-                                                <Image src={`https://www.gravatar.com/avatar/${md5(userActive.username)}?d=identicon`} roundedCircle fluid />
-                                            </Col>
-                                            <Col xs={11}>
-                                                <span><Icon.CircleFill /> {userActive.name}</span><br />
-                                                <small>@{userActive.username}</small>
-                                            </Col>
-                                        </Row>
-                                        <Row className="flex-grow-1 h-100">
-                                            <Col xs={12} className="flex-grow-1 h-100" >
-                                                <div style={{
-                                                    backgroundColor: '#F2F2F2',
-                                                    borderRadius: '1rem',
-                                                    height: 'calc(100% - 70px)',
-                                                    padding: '15px 10px'
-                                                }}>
-                                                    {listMessages.map(message => (
-                                                        <Row className="mb-2">
-                                                            <Col className={
-                                                                `d-flex ${message.sender == user.id ? 'justify-content-end text-end' : ''}`
-                                                            }>
-                                                                <Card className={`${message.sender == user.id ? 'bg-primary text-light' : ''}`}>
-                                                                    <Card.Body>
-                                                                        {message.content}<br />
-                                                                        <small>
-                                                                            {moment(message.timestamp).isSame(moment(), 'day') ? (
-                                                                                "Hoje"
-                                                                            ) : moment(message.timestamp).isSame(moment().subtract(1, 'days'), 'day') ? (
-                                                                                "Ontem"
-                                                                            ) : (
-                                                                                moment(message.timestamp).format('DD/MM/YYYY')
-                                                                            )} - {moment(message.timestamp).format('HH:mm')}
-                                                                        </small>
-                                                                    </Card.Body>
-                                                                </Card>
+                                </Card.Header>
+                                <Card.Body className="m-0">
+                                    <Row className="h-vh-75">
+                                        <Col md={4}>
+                                            <ListGroup>
+                                                {users.map((user) => (
+                                                    <ListGroup.Item className={`${user.id == userActive.id ? 'bg-info text-dark' : ''}`} action onClick={() => handleSelectUser(user)}>
+                                                        <Row>
+                                                            <Col xs={4} lg={2} className="d-flex flex-column justify-content-center">
+                                                                <Image src={`https://www.gravatar.com/avatar/${md5(user.username)}?d=identicon`} roundedCircle fluid />
+                                                            </Col>
+                                                            <Col xs={8} lg={10} className="d-flex flex-column justify-content-center">
+                                                                <span><Icon.CircleFill /> {user.name}</span><br />
+                                                                <small>@{user.username}</small>
                                                             </Col>
                                                         </Row>
-                                                    ))}
-                                                </div>
-                                            </Col>
-                                        </Row>
-                                    </div>
-                                    <InputGroup className="mb-3">
-                                        <Form.Control
-                                            as="textarea"
-                                            placeholder="Mensagem..."
-                                            aria-describedby="send-message"
-                                            style={{ resize: 'none' }}
-                                            value={message}
-                                            onChange={(e) => setMessage(e.target.value)}
-                                        />
-                                        <Button variant="outline-primary" id="send-message" onClick={() => sendMessage()}>
-                                            <Icon.SendFill className="ms-2 me-2 fs-3" />
-                                        </Button>
-                                    </InputGroup>
-                                </Col>
-                            </Row>
-                        </Card.Body>
-                    </Card>
-                </Container>
-            </div>
+                                                    </ListGroup.Item>
+                                                ))}
+                                            </ListGroup>
+                                        </Col>
+                                        <Col md={8} className="d-flex flex-column justify-content-between">
+                                            <div className="flex-grow-1">
+                                                <Row className="mb-2">
+                                                    <Col xs={1} className="d-flex flex-column justify-content-center">
+                                                        <Image src={`https://www.gravatar.com/avatar/${md5(userActive.username)}?d=identicon`} roundedCircle fluid />
+                                                    </Col>
+                                                    <Col xs={11}>
+                                                        <span><Icon.CircleFill /> {userActive.name}</span><br />
+                                                        <small>@{userActive.username}</small>
+                                                    </Col>
+                                                </Row>
+                                                <Row className="flex-grow-1 h-100">
+                                                    <Col xs={12} className="flex-grow-1 h-100" >
+                                                        <div
+                                                            className="overflow-x-hidden overflow-y-auto"
+                                                            style={{
+                                                                backgroundColor: '#F2F2F2',
+                                                                borderRadius: '1rem',
+                                                                height: 'calc(67vh - 70px)',
+                                                                padding: '15px 10px'
+                                                            }}>
+                                                            {listMessages.map(message => (
+                                                                <Row className="mb-2">
+                                                                    <Col className={
+                                                                        `d-flex ${message.sender == user.id ? 'justify-content-end text-end' : ''}`
+                                                                    }>
+                                                                        <Card className={`${message.sender == user.id ? 'bg-primary text-light' : ''}`}>
+                                                                            <Card.Body>
+                                                                                {message.content}<br />
+                                                                                <small>
+                                                                                    {moment(message.timestamp).isSame(moment(), 'day') ? (
+                                                                                        "Hoje"
+                                                                                    ) : moment(message.timestamp).isSame(moment().subtract(1, 'days'), 'day') ? (
+                                                                                        "Ontem"
+                                                                                    ) : (
+                                                                                        moment(message.timestamp).format('DD/MM/YYYY')
+                                                                                    )} - {moment(message.timestamp).format('HH:mm')}
+                                                                                </small>
+                                                                            </Card.Body>
+                                                                        </Card>
+                                                                    </Col>
+                                                                </Row>
+                                                            ))}
+                                                            <div ref={messagesEndRef} />
+                                                        </div>
+                                                    </Col>
+                                                </Row>
+                                            </div>
+                                            <InputGroup className="mb-3">
+                                                <Form.Control
+                                                    as="textarea"
+                                                    placeholder="Mensagem..."
+                                                    aria-describedby="send-message"
+                                                    style={{ resize: 'none' }}
+                                                    value={message}
+                                                    onChange={(e) => setMessage(e.target.value)}
+                                                />
+                                                <Button variant="outline-primary" id="send-message" onClick={() => sendMessage()}>
+                                                    <Icon.SendFill className="ms-2 me-2 fs-3" />
+                                                </Button>
+                                            </InputGroup>
+                                        </Col>
+                                    </Row>
+                                </Card.Body>
+                            </Card>
+                        </Container>
+                    </div>
+            }
         </PrivateRoute>
     );
 }
